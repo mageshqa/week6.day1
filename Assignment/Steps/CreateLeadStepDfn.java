bea92d79a5f51709b1d097dac1085926d52432b1
mageshqa <100154769+mageshqa@users.noreply.github.com>
2022-03-31 22:27:29 +0530

    Update CreateLeadStepDfn.java

--- Assignment/Steps/CreateLeadStepDfn.java
+++ Assignment/Steps/CreateLeadStepDfn.java
@@ -3,6 +3,8 @@ package week6.day1.stepdefn;
 import org.openqa.selenium.By;
 
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.But;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
@@ -20,6 +22,29 @@ public class CreateLeadStepDfn extends W6D1BaseClass {
 	 * new io.cucumber.java.PendingException(); }
 	 */
 
+	@Given("User Enters User name as {string} and Password as {string}")
+	public void user_enters_user_name_as_username_and_password_as_password(String username, String password) throws InterruptedException {
+		driver.findElement(By.id("username")).sendKeys(username);
+		driver.findElement(By.id("password")).sendKeys(password);
+	}
+
+	@And("User Click on Logon Button")
+	public void user_click_on_logon_button() {
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+	}
+
+	@Then("User lands on {string} page")
+	public void user_lands_on_page(String pageText) {
+		boolean pgTxt = driver.findElement(By.xpath("//div[text()='" + pageText + "']")).isDisplayed();
+		System.out.println(pageText + " is " + pgTxt);
+	}
+
+	/*
+	 * @But("Username or Password is incorrect") public void
+	 * Username_or_Password_is_incorrect() {
+	 * System.out.println("Username or Password is incorrect"); }
+	 */
 	@When("User clicks on Lead tab")
 	public void user_clicks_on_lead_tab() {
 		driver.findElement(By.linkText("Leads")).click();
